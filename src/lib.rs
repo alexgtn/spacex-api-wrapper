197f97a547ac330305f1d80bcfb806bad008a493
agutan <alex.gutan.m@gmail.com>
2018-08-18 13:46:29 +0300

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+extern crate reqwest;
+
+pub struct SpaceXAPI {
+}
+
+impl SpaceXAPI {
+    pub fn new() -> SpaceXAPI {
+        SpaceXAPI {}
+    }
+    fn get() {}
+    pub fn get_company_info() {}
+    pub fn get_all_rockets() {}
+    pub fn get_rocket() {}
+    pub fn get_all_capsules() {}
+    pub fn get_capsule() {}
+    pub fn get_all_launch_pads() {}
+    pub fn get_launch_pad() {}
+    pub fn get_latest_launch() {}
+    pub fn get_all_launches() {}
+    pub fn get_all_past_launches() {}
+    pub fn get_all_upcoming_launches() {}
+    pub fn get_all_capsule_parts() {}
+    pub fn get_capsule_part() {}
+    pub fn get_all_core_parts() {}
+    pub fn get_core_part() {}
+}
