fc51e4f034e8f67484654bd77ba04dd258b7c552
agutan <alex.gutan.m@gmail.com>
2018-08-19 20:36:06 +0300

    Refactored. Code Reformat.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,6 @@ extern crate futures;
 extern crate serde;
 
 use serde::Serialize;
-
 use reqwest::Client;
 use reqwest::Response;
 use reqwest::Error;
@@ -11,15 +10,15 @@ use futures::Future;
 use futures::IntoFuture;
 use reqwest::RequestBuilder;
 
-
 pub struct SpaceXAPI {
     settings: Settings
 }
+
 struct Settings {
     version: &'static str,
     host: &'static str,
     ssl: bool,
-    parse_json: bool
+    parse_json: bool,
 }
 
 impl Settings {
@@ -31,7 +30,7 @@ impl Settings {
             version,
             host,
             ssl,
-            parse_json
+            parse_json,
         }
     }
 }
@@ -49,19 +48,17 @@ impl SpaceXAPI {
             Some(true) => true,
             Some(false) => false,
             None => false,
-
         };
         let _parse_json = match parse_json {
             Some(true) => true,
             Some(false) => false,
             None => false,
-
         };
         let settings = Settings {
             version,
-            host : _host,
+            host: _host,
             ssl: _ssl,
-            parse_json: _parse_json
+            parse_json: _parse_json,
         };
 
         SpaceXAPI {
@@ -78,60 +75,75 @@ impl SpaceXAPI {
         let uri = format!("{}{}{}", protocol, hostname, path);
         let mut req_builder = Client::new()
             .get(uri.as_str());
-        let _ : &mut RequestBuilder = match filters {
-            Some(f) => req_builder.query(f),
-            None => &mut req_builder
-        };
+        if filters.is_some() {
+            req_builder.query(filters.unwrap());
+        }
 
         req_builder
             .send()
             .into_future()
     }
+
     fn get<T: Serialize + ?Sized>(&self, path: &str, filters: Option<&T>) -> impl Future<Item=Response, Error=Error> {
         self.request(path, filters)
     }
+
     pub fn get_company_info(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/info", None::<&()>)
     }
+
     pub fn get_all_rockets(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/rockets", None::<&()>)
     }
+
     pub fn get_rocket(&self, id: &'static str) -> impl Future<Item=Response, Error=Error> {
         self.get(format!("/rockets/{}", id).as_str(), None::<&()>)
     }
+
     pub fn get_all_capsules(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/capsules", None::<&()>)
     }
+
     pub fn get_capsule(&self, id: &'static str) -> impl Future<Item=Response, Error=Error> {
         self.get(format!("/capsules/{}", id).as_str(), None::<&()>)
     }
+
     pub fn get_all_launch_pads(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/launchpads", None::<&()>)
     }
+
     pub fn get_launch_pad(&self, id: &'static str) -> impl Future<Item=Response, Error=Error> {
         self.get(format!("/launchpads/{}", id).as_str(), None::<&()>)
     }
+
     pub fn get_latest_launch(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/launches/latest", None::<&()>)
     }
+
     pub fn get_all_launches(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/launches/all", None::<&()>)
     }
+
     pub fn get_all_past_launches(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/launches", None::<&()>)
     }
+
     pub fn get_all_upcoming_launches(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/launches/upcoming", None::<&()>)
     }
+
     pub fn get_all_capsule_parts(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/parts/caps", None::<&()>)
     }
+
     pub fn get_capsule_part(&self, id: &'static str) -> impl Future<Item=Response, Error=Error> {
         self.get(format!("/parts/caps/{}", id).as_str(), None::<&()>)
     }
+
     pub fn get_all_core_parts(&self) -> impl Future<Item=Response, Error=Error> {
         self.get("/parts/cores", None::<&()>)
     }
+
     pub fn get_core_part(&self, id: &'static str) -> impl Future<Item=Response, Error=Error> {
         self.get(format!("/parts/cores/{}", id).as_str(), None::<&()>)
     }
